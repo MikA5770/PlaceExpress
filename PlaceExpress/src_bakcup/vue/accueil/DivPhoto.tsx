6de392bb87f4d36b14f1e4737631127d4b582c00
MikA5770 <148467318+MikA5770@users.noreply.github.com>
2024-07-24 17:48:50 +0200

    Delete PlaceExpress/src_bakcup directory

--- PlaceExpress/src_bakcup/vue/accueil/DivPhoto.tsx
+++
@@ -1,8 +0,0 @@
-export function DivPhoto(value:{ cheminGrandeImg : string, cheminPetiteImg : string}){
-    return(
-      <div className="accueil_divPhoto">
-        <img className="accueil_divPhoto_petite" src={value.cheminPetiteImg} />
-        <img className="accueil_divPhoto_grande" src={`${value.cheminGrandeImg}`} />
-      </div>
-    )
-}
